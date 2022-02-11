5b52336642d478aa386b4abfe3e9dd342a4b5bc6
ghp <ghp418@163.com>
2022-02-11 20:33:29 +0800

    add BindingAdapter

--- databinding/src/main/java/com/ghp/databinding/data/DataBindingViewModel.kt
+++ databinding/src/main/java/com/ghp/databinding/data/DataBindingViewModel.kt
@@ -8,6 +8,7 @@ import androidx.lifecycle.*
 import com.ghp.databinding.R
 import com.ghp.databinding.util.BindableTestModel
 import com.ghp.databinding.util.ObservableViewModel
+import com.ghp.databinding.util.Popularity
 
 class DataBindingViewModel : ObservableViewModel() {
 
@@ -25,6 +26,17 @@ class DataBindingViewModel : ObservableViewModel() {
     val bindingAdapterImageUrl = MutableLiveData("https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fww3.sinaimg.cn%2Fmw690%2F71b89a12gy1gz51ovv9zpj20zk144go4.jpg&refer=http%3A%2F%2Fwww.sina.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1647153265&t=a2b323af7e5fea6424e893f8518de562")
     val bindingAdapterImagePlaceholder = R.mipmap.image_placeholder
 
+    private val _likes =  MutableLiveData(0)
+    val likes: LiveData<Int> = _likes
+    // popularity is exposed as LiveData using a Transformation instead of a @Bindable property.
+    val popularity: LiveData<Popularity> = Transformations.map(_likes) {
+        when {
+            it > 9 -> Popularity.STAR
+            it > 4 -> Popularity.POPULAR
+            else -> Popularity.NORMAL
+        }
+    }
+
     //Bindable test
     fun imageClick(view: View) {
         bindableTestModel.firstName = "bindableTestModel"
@@ -33,6 +45,7 @@ class DataBindingViewModel : ObservableViewModel() {
 
     //click test
     fun onButtonClick(view: View){
+        _likes.value = (_likes.value ?: 0) + 1
         Toast.makeText(view.context, "onButtonClick", Toast.LENGTH_SHORT).show()
     }
 
@@ -50,4 +63,6 @@ class DataBindingViewModel : ObservableViewModel() {
         isShowImgView.value = isChecked
         Toast.makeText(buttonView.context, "onCheckChanged isChecked $isChecked", Toast.LENGTH_SHORT).show()
     }
+
+
 }
--- databinding/src/main/java/com/ghp/databinding/util/BindingAnnotation.kt
+++ databinding/src/main/java/com/ghp/databinding/util/BindingAnnotation.kt
@@ -2,17 +2,27 @@ package com.ghp.databinding.util
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.res.ColorStateList
 import android.graphics.drawable.Drawable
+import android.os.Build
 import android.util.AttributeSet
 import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.ContextCompat
 import androidx.databinding.*
 import com.bumptech.glide.Glide
 import com.ghp.databinding.BR
 import com.ghp.databinding.R
+
+enum class Popularity {
+    NORMAL,
+    POPULAR,
+    STAR
+}
 
 object BindingAnnotation {
     /**
@@ -32,6 +42,20 @@ object BindingAnnotation {
             .into(view)
     }
 
+    @BindingAdapter("app:progressTint")
+    @JvmStatic fun tintPopularity(view: ProgressBar, popularity: Popularity) {
+
+        val color = getAssociatedColor(popularity, view.context)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            view.progressTintList = ColorStateList.valueOf(color)
+        }
+    }
+    @BindingAdapter(value = ["app:progressScaled", "android:max"], requireAll = true)
+    @JvmStatic fun setProgress(progressBar: ProgressBar, likes: Int, max: Int) {
+        progressBar.progress = (likes * max / 20).coerceAtMost(max)
+    }
+
 //    //全局改变，慎用
 //    //android:visibility="@{boolean}"
 //    @BindingConversion
@@ -40,7 +64,14 @@ object BindingAnnotation {
 //    }
 
 }
-
+private fun getAssociatedColor(popularity: Popularity, context: Context): Int {
+    return when (popularity) {
+        Popularity.NORMAL -> context.theme.obtainStyledAttributes(
+            intArrayOf(android.R.attr.colorForeground)).getColor(0, 0x000000)
+        Popularity.POPULAR -> ContextCompat.getColor(context, R.color.popular)
+        Popularity.STAR -> ContextCompat.getColor(context, R.color.star)
+    }
+}
 
 // 需添加'kotlin-kapt'
 //自定义属性绑定方法
